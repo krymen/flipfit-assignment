6a0c966bc179da610f7f8bb7f8db9342b7413c19
Krzysztof Menżyk <krzysztof@menzyk.net>
2019-11-06 13:23:11 +0100

    Setup Typescript and Jest in SortAndSearch

--- SortAndSearch/index.ts
+++ SortAndSearch/index.ts
@@ -1,5 +1,5 @@
-import { ASort } from './ASort';
-import { BSort } from './BSort';
+import { ASort } from './src/ASort';
+import { BSort } from './src/BSort';
 
 const unsorted = [13, 2, 17, 5, 77, 22, 83, 65, 14, 9, 0, 4, 7, 32];
 const elementsToFind = [1, 5, 13, 27, 77];
